728a5c2546df407f0d00e263e2e05e7a766252c4
MattiaPortanti <mattia.portantipm@gmail.com>
2022-03-17 17:16:23 +0100

    importate immegini

--- js/script.js
+++ js/script.js
@@ -1,4 +1,40 @@
 const root = new Vue({
     
     el: "#root",
+    data: {
+        slider : [
+
+            {
+                url:'https://www.mlaworld.com/wp-content/uploads/2019/09/toronto-2.jpg',
+                title:'Toronto',
+                text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+        
+            {
+                url: "https://wise.com/imaginary/costo-della-vita-a-singaporeuno.jpg",
+                title:'Singapore',
+                text:'Lorem ipsum',
+            },
+        
+            {
+                url: "https://www.turismoroma.it/sites/default/files/Colosseo_0_0.jpg",
+                title:'Roma',
+                text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+        
+            {
+                url: "https://static2-viaggi.corriereobjects.it/wp-content/uploads/2020/11/new-york.jpg?v=409121",
+                title:'New York',
+                text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+        
+            {
+                url: "https://www.lumi4innovation.it/app/uploads/2021/03/Green-city-nuovi-modelli-di-citt%C3%A0-sostenibile-1024x516.png",
+                title:'Paradise',
+                text:'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+        ],
+
+        
+    }
 });
